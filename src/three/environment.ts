a92e23e56359234b3a936502bf4ba9770e116e17
calima <filippo.chioccini@gmail.com>
2026-05-11 11:56:22 +0200

    fix deploy

--- src/three/environment.ts
+++ src/three/environment.ts
@@ -21,9 +21,9 @@ export async function loadEnvironment(scene: Scene, renderer: WebGPURenderer) {
   
   const base = import.meta.env.BASE_URL 
   const [sdr, gainMap, metadata] = await Promise.all([
-    loader.loadAsync(base + 'hdr.jpg'),
-    loader.loadAsync(base + 'hdr-gainmap.jpg'),
-    fetch(base + 'hdr.json').then(r => r.json())
+    loader.loadAsync(base + 'hdrPink.jpg'),
+    loader.loadAsync(base + 'hdrPink-gainmap.jpg'),
+    fetch(base + 'hdrPink.json').then(r => r.json())
   ])
 
   sdr.colorSpace = SRGBColorSpace
